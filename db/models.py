7a7876cd9518c71de88070407df342f0a9866699
ayoub <gholami.ayub73@gmail.com>
2023-03-07 10:13:46 +0330

    first commit.

---
+++ db/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Programs(models.Model):
+    data = models.JSONField(null=True)
+
+
+
+
---
+++ settings.py
@@ -0,0 +1,38 @@
+import os
+
+# This defines the base dir for all relative imports for our project, put the file in your root folder so the
+# base_dir points to the root folder
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+SECRET_KEY = 'LcULz8NMxj~za0[#gp.h4])1frv[jwasLcULz8NMxj~za0[#gp'
+# According to your data file, you can change the engine, like mysql, postgresql, mongodb etc make sure your data is
+# directly placed in the same folder as this file, if it is not, please direct the 'NAME' field to its actual path.
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#     }
+# }
+
+
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432
+    }
+}
+
+#Since we only have one app which we use
+INSTALLED_APPS = (
+    'db',
+)
+
+# Write a random secret key here
