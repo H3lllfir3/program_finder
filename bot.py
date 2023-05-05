e15c949989aece6494e58142caa62c20819c7cc3
H3lllfir3 <gholami.ayub73+github@gmail.com>
2023-05-05 12:07:53 +0200

    use webhook in bot.

--- bot.py
+++ bot.py
@@ -1,56 +1,29 @@
-import discord
-import logging
-import jdatetime
+import requests
+import json
 
+class DiscordWebhook:
+    def __init__(self, webhook_url):
+        self.webhook_url = webhook_url
+        self.headers = {
+            "Content-Type": "application/json"
+        }
 
-class DiscordClient:
-    def __init__(self, token, channel_id, log_handler, log_level=logging.DEBUG):
-        self.token = token
-        self.channel_id = channel_id
-        self.log_handler = log_handler
-        self.log_level = log_level
-        self.client = discord.Client(intents=discord.Intents.default())
-        self.logger = logging.getLogger(__name__)
-        self.logger.addHandler(self.log_handler)
-        self.logger.setLevel(self.log_level)
+    def send_message(self, content, username=None, avatar_url=None):
+        payload = {
+            "content": content
+        }
 
-    async def send_data_to_discord(self, lst, log_messages=None):
-        print(1)
-        self.logger.info('Sending data to discord...')
-        print(2)
-        print(self.token)
-        # await self.client.start(self.token)
-        print(3)
+        response = requests.post(self.webhook_url, data=json.dumps(payload), headers=self.headers)
 
-        @self.client.event
-        async def on_ready():
-            print(4)
-            self.logger.info('Bot is ready!')
-            print(5)
-            channel = self.client.get_channel(self.channel_id)
-            time = jdatetime.datetime.now().strftime("%a, %d %b %Y %H:%M")
-            if lst:
-                await channel.send(f"***New program added at {time}***")
-            
-                for data in lst:
-                    self.logger.info(f"Sending data to discord - {data}")
-                    msg = f"""
-                        ***Platform***: {data['platform']}
-                        ***Name***: {data['program_name']}
-                        ***Company***: {data['company_name']}
-                        ***URL***: {data['program_url']}\n\n
-                    """
-                    await channel.send(msg)
-            # if log_messages:
-            #     log_channel = self.client.get_channel(1083988375108866048)
-            #     for msg in log_messages:
-            #         await log_channel.send(f"***{msg}***")
+        if response.status_code == 204:
+            print("Message sent successfully.")
+        else:
+            print(f"Failed to send message. Status code: {response.status_code}")
+            print(response.text)
 
-            if not lst:
-                log_channel = self.client.get_channel(1083988375108866048)
-                await log_channel.send(f"***No data found at {time}***")
+if __name__ == "__main__":
+    webhook_url = "YOUR_WEBHOOK_URL"  # Replace this with your webhook URL
+    message_content = "Hello, Discord!"  # Replace this with the message you want to send
 
-            await self.client.close()
-        print('im at the end.')
-        await self.client.run(self.token)
-
+    discord_webhook = DiscordWebhook(webhook_url)
+    discord_webhook.send_message(message_content, username="MyBot", avatar_url=None)
